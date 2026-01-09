9aec3e65c685104ca6af0d8406133a1130dd0756
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-01-09 19:40:09 +0530

    Time: 11 ms (34.97%) | Memory: 7.6 MB (87.20%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+        //  if(n == 1){
+        //     return 1;
+        // }
+        return fib(n - 1)+ fib(n - 2);
+    }
+};

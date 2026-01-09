9bd5811d115ec72e0a3a2f69fc5aa5f3b2d344a7
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-01-09 19:48:41 +0530

    Time: 0 ms (100%), Space: 8 MB (14.08%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,13 +3,13 @@ public:
     bool isPowerOfTwo(int n) {
 
         int ans = 1;
-        for (int i = 0 ; i <= 30 ; i++){
-            
-            if(ans == n){
+        for (int i = 0; i <= 30; i++) {
+
+            if (ans == n) {
                 return true;
             }
-            if(ans < INT_MAX /2)
-            ans = ans * 2;
+            if (ans < INT_MAX / 2)
+                ans = ans * 2; // it's basically increasing the numberset
         }
         return false;
     }

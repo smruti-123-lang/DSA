9a14e99cac289ea2c0ca2f8481124406be1175e7
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-01-17 12:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (69.56%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    void solve(vector<int>& ds, vector<int>& freq,
-               vector<vector<int>>& ans, vector<int>& nums) {
-
-        if (ds.size() == nums.size()) {
-            ans.push_back(ds);
+    void solve(int index,vector<int>& nums, vector<vector<int>>& ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
             return;
         }
+        for(int i = index;i < nums.size();i++){
+            swap(nums[i],nums[index]);
+            solve(index + 1,nums,ans);
+            swap(nums[i],nums[index]);
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (!freq[i]) {
-                freq[i] = 1;
-                ds.push_back(nums[i]);
 
-                solve(ds, freq, ans, nums);
-
-                ds.pop_back();
-                freq[i] = 0;
-            }
         }
     }
+
+    // void solve(vector<int>& ds, vector<int>& freq,
+    //            vector<vector<int>>& ans, vector<int>& nums) {
+
+    //     // if (ds.size() == nums.size()) {
+    //     //     ans.push_back(ds);
+    //     //     return;
+    //     // }
+
+    //     // for (int i = 0; i < nums.size(); i++) {
+    //     //     if (!freq[i]) {
+    //     //         freq[i] = 1;
+    //     //         ds.push_back(nums[i]);
+
+    //     //         solve(ds, freq, ans, nums);
+
+    //     //         ds.pop_back();
+    //     //         freq[i] = 0;
+    //     //     }
+    //     // }
+
+    // }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
-        vector<int> freq(nums.size(), 0);
 
-        solve(ds, freq, ans, nums);
+        solve(0,nums,ans);
         return ans;
     }
 };

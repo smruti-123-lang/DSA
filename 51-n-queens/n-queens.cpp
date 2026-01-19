220017cbf0331d93787b028b3417cecc7cd6f9d3
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-01-19 17:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (76.16%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void solve(int col,
+               vector<string> &board,
+               vector<vector<string>> &ans,
+               vector<int> &leftrow,
+               vector<int> &upperdiagonal,
+               vector<int> &lowerdiagonal,
+               int n) {
+
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+
+            if (leftrow[row] == 0 &&
+                upperdiagonal[n - 1 + col - row] == 0 &&
+                lowerdiagonal[row + col] == 0) {
+
+                board[row][col] = 'Q';
+                leftrow[row] = 1;
+                upperdiagonal[n - 1 + col - row] = 1;
+                lowerdiagonal[row + col] = 1;
+
+                solve(col + 1, board, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                upperdiagonal[n - 1 + col - row] = 0;
+                lowerdiagonal[row + col] = 0;
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        vector<int> leftrow(n, 0);
+        vector<int> upperdiagonal(2 * n - 1, 0);
+        vector<int> lowerdiagonal(2 * n - 1, 0);
+
+        solve(0, board, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+        return ans;
+    }
+};

1dc542c4acc666218a73453acf620e5b182c5207
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-01-19 17:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (73.89%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-int count = 0;
-    void solve(int col, vector<string>& board, vector<vector<string>>& ans,
-               vector<int>& leftrow, vector<int>& upperdiagonal,
-               vector<int>& lowerdiagonal, int n) {
+    int count = 0;
+
+    void solve(int col,
+               vector<int> &leftrow,
+               vector<int> &upperdiagonal,
+               vector<int> &lowerdiagonal,
+               int n) {
+
         if (col == n) {
-            ans.push_back(board);
             count++;
             return;
         }
+
         for (int row = 0; row < n; row++) {
-            if (leftrow[row] == 0 && upperdiagonal[n - 1 + col - row] == 0 &&
+
+            if (leftrow[row] == 0 &&
+                upperdiagonal[n - 1 + col - row] == 0 &&
                 lowerdiagonal[row + col] == 0) {
-                board[row][col] = 'Q';
+
                 leftrow[row] = 1;
                 upperdiagonal[n - 1 + col - row] = 1;
                 lowerdiagonal[row + col] = 1;
-                solve(col + 1, board, ans, leftrow, upperdiagonal,
-                      lowerdiagonal, n);
-                board[row][col] = '.';
+
+                solve(col + 1, leftrow, upperdiagonal, lowerdiagonal, n);
+
                 leftrow[row] = 0;
                 upperdiagonal[n - 1 + col - row] = 0;
                 lowerdiagonal[row + col] = 0;
             }
         }
     }
+
     int totalNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n, string(n, '.'));
+
         vector<int> leftrow(n, 0);
         vector<int> upperdiagonal(2 * n - 1, 0);
         vector<int> lowerdiagonal(2 * n - 1, 0);
-        solve(0, board, ans, leftrow, upperdiagonal, lowerdiagonal, n);
+
+        solve(0, leftrow, upperdiagonal, lowerdiagonal, n);
         return count;
     }
-}
-;
+};

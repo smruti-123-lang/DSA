033bf032b91f67d006b49749988ce26315345a1c
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-02-21 15:31:50 +0530

    Time: 3 ms (72.17%) | Memory: 7.9 MB (88.15%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,5 +1,4 @@
 #include <bitset>
-#include <vector>
 using namespace std;
 
 class Solution {
@@ -13,25 +12,13 @@ public:
     }
 
     int countPrimeSetBits(int left, int right) {
-        int n = right - left + 1;
-        vector<int> count(n, 0);  // array to store set-bit counts
+        int ans = 0;
 
-        int k = 0;
         for(int i = left; i <= right; i++){
             bitset<32> b(i);
-            string s = b.to_string();
+            int cnt = b.count();   // directly count set bits
 
-            for(int j = 0; j < 32; j++){
-                if(s[j] == '1'){
-                    count[k]++;   // count set bits for this number
-                }
-            }
-            k++;
-        }
-
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            if(isPrime(count[i])) ans++;  // check prime
+            if(isPrime(cnt)) ans++;
         }
 
         return ans;

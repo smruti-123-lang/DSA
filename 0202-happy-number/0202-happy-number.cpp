392b13af2fe0eb66f00d0a49d1b15a2cb7c1781a
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-03-10 23:59:11 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.22%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     bool isHappy(int n) {
 
-        unordered_set<int> seen;
+        while(n != 1 && n != 4){   // 4 means it will enter unhappy cycle
 
-        while(n != 1 && seen.count(n) == 0){
-
-            seen.insert(n);
             int sum = 0;
 
             while(n > 0){
                 int digit = n % 10;
-                sum = sum + pow(digit,2);
-                n = n /10;
+                sum = sum + digit * digit;
+                n = n / 10;
             }
+
             n = sum;
-
-
         }
-        return n==1;
 
-
+        return n == 1;
     }
 };

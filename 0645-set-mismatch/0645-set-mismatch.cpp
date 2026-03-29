0840dc644e85d390d41d44829a96f418f63e0788
Smrutishree Misra <smrutishreemisra@gmail.com>
2026-03-29 20:37:55 +0530

    Time: 27 ms (26.99%), Space: 34.8 MB (7.8%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,32 +3,22 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         unordered_map<int,int> hash;
         vector<int> ans(2);
-
+        
         // Count frequency
         for(int i = 0; i < nums.size(); i++){
             hash[nums[i]]++;
         }
-
-        int duplicate = -1, missing = -1;
-
-        // Find duplicate
-        for(auto it : hash){
-            if(it.second == 2){
-                duplicate = it.first;
-            }
-           
-        }
-
-        // Find missing
+        
+        // Find duplicate and missing
         for(int i = 1; i <= nums.size(); i++){
-            if(hash[i] == 0){
-                missing = i;
+            if(hash[i] == 2){
+                ans[0] = i; // duplicate
+            }
+            else if(hash[i] == 0){
+                ans[1] = i; // missing
             }
         }
-
-        ans[0] = duplicate;
-        ans[1] = missing;
-
+        
         return ans;
     }
 };
